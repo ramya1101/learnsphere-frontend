dfc5dbaf0d6483b31c3283c2ae2beb6fd159e047
ramya1101 <ramyaaa876@gmail.com>
2025-08-30 11:14:52 +0530

    version1.1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+//this is app.ts
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
